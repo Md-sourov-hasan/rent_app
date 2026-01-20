0ff7fde01ca9b6a36ef64f29f7fd86e15ef1d51b
Md Sourov Hasan <mdsourobhasan2552@gmail.com>
2026-01-20 12:00:16 +0600

    homepahge dollar slot

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -14,7 +14,12 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.only(top: 40,left: 10,right: 10,bottom: 16),
+        padding: const EdgeInsets.only(
+          top: 40,
+          left: 10,
+          right: 10,
+          bottom: 16,
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.start,
@@ -22,7 +27,11 @@ class _HomeScreenState extends State<HomeScreen> {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Image.asset('assets/images/png/splash.png', width: 50,height: 50,),
+                Image.asset(
+                  'assets/images/png/splash.png',
+                  width: 50,
+                  height: 50,
+                ),
                 Container(
                   child: Row(
                     children: [
@@ -35,10 +44,16 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                         child: InkWell(
                           borderRadius: BorderRadius.circular(40),
-                          child: Center(child: Image.asset("assets/images/png/notification.png",width: 20,height: 20,)),
+                          child: Center(
+                            child: Image.asset(
+                              "assets/images/png/notification.png",
+                              width: 20,
+                              height: 20,
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(width: 10),
                       Container(
                         height: 35,
                         width: 35,
@@ -48,10 +63,17 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                         child: InkWell(
                           borderRadius: BorderRadius.circular(40),
-                          child: Center(child: Image.asset("assets/images/png/setting.png",width: 20,height: 20,)),
+                          child: Center(
+                            child: Image.asset(
+                              "assets/images/png/setting.png",
+                              width: 20,
+                              height: 20,
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(width: 10,),Container(
+                      SizedBox(width: 10),
+                      Container(
                         height: 35,
                         width: 35,
                         decoration: BoxDecoration(
@@ -60,51 +82,61 @@ class _HomeScreenState extends State<HomeScreen> {
                         ),
                         child: InkWell(
                           borderRadius: BorderRadius.circular(40),
-                          child: Center(child: Image.asset("assets/images/png/profile_image.png",fit: BoxFit.cover,)),
+                          child: Center(
+                            child: Image.asset(
+                              "assets/images/png/profile_image.png",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                       ),
-                      SizedBox(width: 10,),
+                      SizedBox(width: 10),
                     ],
                   ),
-                )
-                ],
+                ),
+              ],
             ),
-            SizedBox(height: 20,),
-            Text('Welcome Mr. Johnson!',
-            style: TextStyle(
-              color: Color(0xff1C1C1C),
-              fontSize: AllSizes.medium,
-              fontWeight: FontWeight.bold,
+            SizedBox(height: 20),
+            Text(
+              'Welcome Mr. Johnson!',
+              style: TextStyle(
+                color: Color(0xff1C1C1C),
+                fontSize: AllSizes.medium,
+                fontWeight: FontWeight.bold,
+              ),
             ),
+            SizedBox(height: 5),
+            Text(
+              'Manage your real estate properties & tenants',
+              style: TextStyle(
+                color: Color(0xff1C1C1C).withOpacity(0.3),
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            SizedBox(height: 5,),
-            Text('Manage your real estate properties & tenants',
-            style: TextStyle(
-              color: Color(0xff1C1C1C).withOpacity(0.3),
-              fontSize: 14,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            SizedBox(height: 20,),
+            SizedBox(height: 20),
             Row(
               children: [
                 Expanded(
                   child: Container(
-                    height:150,
+                    height: 150,
                     decoration: BoxDecoration(
-                      boxShadow:[
+                      boxShadow: [
                         BoxShadow(
                           color: Colors.black38.withOpacity(0.1),
                           blurRadius: 5,
-                          spreadRadius:1,
-                          offset: Offset(0, 4)
-                        )
+                          spreadRadius: 1,
+                          offset: Offset(0, 4),
+                        ),
                       ],
                       color: AllColors.secondaryColor,
-                      border: Border.all(width: 1,color: Colors.black.withOpacity(0.1)),
-                      borderRadius: BorderRadius.circular(20)
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.1),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                    child:Padding(
+                    child: Padding(
                       padding: const EdgeInsets.all(16.0),
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.start,
@@ -116,160 +148,262 @@ class _HomeScreenState extends State<HomeScreen> {
                             decoration: BoxDecoration(
                               borderRadius: BorderRadius.circular(10),
                             ),
-                            child: Image.asset('assets/images/png/person.png',
-                            fit: BoxFit.cover,
+                            child: Image.asset(
+                              'assets/images/png/person.png',
+                              fit: BoxFit.cover,
                             ),
                           ),
-                          Text('Total Tenants',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold,
+                          SizedBox(height: 5),
+                          Text(
+                            'Total Tenants',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
+                          SizedBox(height: 5),
+                          Text(
+                            '24',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '+3 this month',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black.withOpacity(0.3),
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
                         ],
                       ),
                     ),
-                  )
                   ),
+                ),
 
-
-
-
-
-
-
-
-
-
-                  SizedBox(width: 10,),
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                  Expanded(
-                  child: Container(
-                    height:150,
-                    decoration: BoxDecoration(
-                      boxShadow:[
-                        BoxShadow(
-                          color: Colors.black38.withOpacity(0.1),
-                          blurRadius: 5,
-                          spreadRadius:1,
-                          offset: Offset(0, 4)
-                        )
-                      ],
-                      color: AllColors.secondaryColor,
-                      border: Border.all(width: 1,color: Colors.black.withOpacity(0.1)),
-                      borderRadius: BorderRadius.circular(20)
-                    ),
-                  )
-                  ),
-
-
-
-
-
-
-              ],
-            ),
-
-
-
-
-
-            SizedBox(height: 10,),
-
-
-
-
-
-
-
-            Row(
-              children: [
-
-
-
+                SizedBox(width: 10),
 
                 Expanded(
                   child: Container(
-                    height:150,
+                    height: 150,
                     decoration: BoxDecoration(
-                      boxShadow:[
+                      boxShadow: [
                         BoxShadow(
                           color: Colors.black38.withOpacity(0.1),
                           blurRadius: 5,
-                          spreadRadius:1,
-                          offset: Offset(0, 4)
-                        )
+                          spreadRadius: 1,
+                          offset: Offset(0, 4),
+                        ),
                       ],
                       color: AllColors.secondaryColor,
-                      border: Border.all(width: 1,color: Colors.black.withOpacity(0.1)),
-                      borderRadius: BorderRadius.circular(20)
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.1),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
                     ),
-                  )
-                  ),
-
-
-
-
-
-
-
-
-                  SizedBox(width: 10,),
-
-
-
-
-
-
-
-
-                  Expanded(
-                  child: Container(
-                    height:150,
-                    decoration: BoxDecoration(
-                      boxShadow:[
-                        BoxShadow(
-                          color: Colors.black38.withOpacity(0.1),
-                          blurRadius: 5,
-                          spreadRadius:1,
-                          offset: Offset(0, 4)
-                        )
-                      ],
-                      color: AllColors.secondaryColor,
-                      border: Border.all(width: 1,color: Colors.black.withOpacity(0.1)),
-                      borderRadius: BorderRadius.circular(20)
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Image.asset(
+                              'assets/images/png/dollar.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            'Rent Due',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '\$12,400',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '5 pending',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black.withOpacity(0.3),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  )
                   ),
-
-
-
-
-
-
-
-
-
+                ),
               ],
-            )
+            ),
+
+            SizedBox(height: 10),
+
+            Row(
+              children: [
+                Expanded(
+                  child: Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black38.withOpacity(0.1),
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                      color: AllColors.secondaryColor,
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.1),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Image.asset(
+                              'assets/images/png/person.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            'Total Tenants',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '24',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '+3 this month',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black.withOpacity(0.3),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+
+                SizedBox(width: 10),
+
+                Expanded(
+                  child: Container(
+                    height: 150,
+                    decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.black38.withOpacity(0.1),
+                          blurRadius: 5,
+                          spreadRadius: 1,
+                          offset: Offset(0, 4),
+                        ),
+                      ],
+                      color: AllColors.secondaryColor,
+                      border: Border.all(
+                        width: 1,
+                        color: Colors.black.withOpacity(0.1),
+                      ),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Container(
+                            height: 30,
+                            width: 30,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                            child: Image.asset(
+                              'assets/images/png/person.png',
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            'Total Tenants',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '24',
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Colors.black,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                          Text(
+                            '+3 this month',
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.black.withOpacity(0.3),
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
