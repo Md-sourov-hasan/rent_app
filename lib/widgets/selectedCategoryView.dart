d637b1a300b755fdefb9c877e2410a76f4bbccda
Md Sourov Hasan <mdsourobhasan2552@gmail.com>
2026-01-23 22:54:23 +0600

    23

--- lib/widgets/selectedCategoryView.dart
+++ lib/widgets/selectedCategoryView.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:rent_app/const/all_colors.dart';
-import 'package:rent_app/const/all_sizes.dart';
 
 selectedCategoryView(Map<String, String> category) {
   return Padding(
