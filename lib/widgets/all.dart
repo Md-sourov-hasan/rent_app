d45a45dc88aaa6e716154482bd697c604b677bd7
Md Sourov Hasan <mdsourobhasan2552@gmail.com>
2026-01-24 09:40:22 +0600

    Add paid rent indicator with icon to All widget

--- lib/widgets/all.dart
+++ lib/widgets/all.dart
@@ -10,12 +10,47 @@ class All extends StatefulWidget {
 class _AllState extends State<All> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(height: 100,),
-        Container(height: 100,),
-        Container(height: 100,),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 16),
+                child: Container(
+                  height: 30,
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Color(0xffF0FDF4)
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset('assets/images/png/paid_icon.png',
+                      height: 12,
+                      width: 12,
+                      ),
+                      SizedBox(width: 5,),
+                      Text('Paid rent',
+                      style: TextStyle(
+                        color: Color(0xff00aa8e)
+                      ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 10,),
+          SizedBox(width: 10,),
+          Text('Sunset Apartments A-301')
+          
+        ],
+      ),
     );
           
        
